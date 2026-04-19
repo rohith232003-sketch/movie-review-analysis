841e586addafc3cfb9d89a2e246bb6007ecc8e91
rohith232003-sketch <rohith232003@gmail.com>
2026-04-19 10:45:05 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,37 +1,35 @@
 import streamlit as st
 import requests
 
-#page config
 st.set_page_config(
     page_title="Sentiment Analysis",
     page_icon="🎥",
-    layout="centered",      
+    layout="centered",
 )
 
 st.title("Movie Review Sentiment Analysis")
 st.write("Enter a movie review and get instant sentiment prediction")
 
-#input box
 review = st.text_area("Enter your movie review here:", height=150)
 
-#predict button
 if st.button("Analyze Sentiment"):
     if review.strip() == "":
         st.warning("Please enter a review first")
     else:
         with st.spinner("Analyzing..."):
-            # Call your Flask backend
-            response = requests.post(
-                "http://127.0.0.1:5000/predict",
-                json={"reviews": review}
-            )
-            result = response.json()
+            try:
+                response = requests.post(
+                    "https://movie-review-analysis-13.onrender.com/predict",
+                    json={"reviews": review}
+                )
+                result = response.json()
+            except:
+                st.error("Backend not running or connection failed")
+                st.stop()
 
-        #Display result
-        sentiment = result['prediction']
-        #confidence = result['confidence']
+        sentiment = result.get('prediction', 'Unknown')
 
-        if "pos" in sentiment or "neg" in sentiment:
+        if sentiment.lower() in ["positive", "negative"]:
             st.success(f"Sentiment: {sentiment}")
         else:
-            st.error(f"entiment: {sentiment}")
+            st.error(f"Sentiment: {sentiment}")
